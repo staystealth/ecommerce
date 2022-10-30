572c0cf3a7f5cb72f3f0685a847210d64598cbdf
staystealth <wsinc01@protonmail.com>
2022-10-30 12:50:11 +0000

    (statecontext, cart) create, slug update

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,18 +3,19 @@ import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-
 
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 //import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  //const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
   const handleBuyNow = () => {
     onAdd(product, qty);
 
     setShowCart(true);
   }
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -55,9 +56,9 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick=""><AiOutlineMinus /></span>
-              <span className="num">1</span>
-              <span className="plus" onClick=""><AiOutlinePlus /></span>
+              <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className="buttons">
